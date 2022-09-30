2730c06cfe531e55c8342e669b237a2d270cdac8
Thibode <t.dequidt@empreintedigitale.coop>
2022-09-30 15:30:26 +0200

    Correction formulaire

--- src/Form/FormPotentielClientType.php
+++ src/Form/FormPotentielClientType.php
@@ -25,25 +25,35 @@ class FormPotentielClientType extends AbstractType
                 'attr' => ['tabindex' => 2]
             ])
             ->add('email', TextType::class, [
+                'label_format' => 'Email ',
                 'attr' => ['tabindex' => 3]
             ])
             ->add('phone', TextType::class, [
+                'label_format' => 'Téléphone : ',
                 'attr' => ['tabindex' => 4]
             ])
             ->add('opticien',EntityType::class, [
+                'label_format' => 'Opticien : ',
                 'class' => Opticiens::class,
                 'attr' => ['tabindex' => 6]
             ])
             ->add('dateTest', DateType::class, [
-                'attr' => ['tabindex' => 5]
+                'label_format' => 'Date du test : ',
+                'attr' => ['tabindex' => 5],
+                'widget' => 'single_text',
+                'required' => false,
+                'mapped' => false,
+                'html5' => true,
             ])
             ->add('appareil', ChoiceType::class, [
+                'label_format' => 'Appareillé(e) : ',
                 'choices' => [
                     'OUI' => PotentielClient::OUI,
                     'NON' => PotentielClient::NON,
                 ], 'attr' => ['tabindex' => 8]
             ])
             ->add('perteAuditive', ChoiceType::class, [
+                'label_format' => 'Perte auditive : ',
                 'choices' => [
                     'OUI' => PotentielClient::OUI,
                     'NON' => PotentielClient::NON,
@@ -51,18 +61,21 @@ class FormPotentielClientType extends AbstractType
                 ],  'attr' => ['tabindex' => 7]
             ])
             ->add('deni', ChoiceType::class, [
+                'label_format' => 'Déni : ',
                 'choices' => [
                     'OUI' => PotentielClient::OUI,
                     'NON' => PotentielClient::NON,
                 ], 'attr' => ['tabindex' => 10]
             ])
             ->add('souhait', ChoiceType::class, [
+                'label_format' => 'Souhait : ',
                 'choices' => [
                     'OUI' => PotentielClient::OUI,
                     'NON' => PotentielClient::NON,
                 ],  'attr' => ['tabindex' => 9]
             ])
             ->add('remarque', TextareaType::class, [
+                'label_format' => 'Remarque : ',
                 'attr' => [
                     'cols' => 50,
                     'rows' => 5
